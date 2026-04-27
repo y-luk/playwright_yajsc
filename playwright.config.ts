77e241ebb34259cb3fb2224a1a9df34daa07d0f2
y-luk <julia.la753@gmail.com>
2026-04-28 00:13:24 +0200

    Unit-10

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  expect: {
+    timeout: 10_000,
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
