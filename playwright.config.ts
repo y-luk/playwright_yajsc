f831b92321b5c570144e8c7ab68f748aae121da9
y-luk <julia.la753@gmail.com>
2026-05-28 01:14:02 +0200

    Unit-14

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  expect: { timeout: 10000 },
+  expect: { timeout: 20000 },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
